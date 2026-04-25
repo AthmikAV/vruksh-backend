e6dff686ff1e036bcc89f35e8a1d6f00341508a3
AthmikAV <athmikadpangaya@gmail.com>
2026-04-25 17:49:31 +0530

    fix cookie secure for cross-origin

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res,next) => {
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
         res.cookie('token', token, {
             httpOnly: true,   
-            secure: process.env.NODE_ENV === 'production',    
+            secure: true,    
            sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000 
         })
